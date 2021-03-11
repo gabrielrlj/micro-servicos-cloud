7e02068e34f4e9429608b61cb267abe9ff1a9337
Gabriel Jardim <gabrielrlj99@outlook.com>
2021-03-11 15:07:58 -0300

    ribbon load balancing

--- hr-payroll/src/main/java/br/jardim/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/br/jardim/hrpayroll/HrPayrollApplication.java
@@ -2,9 +2,12 @@ package br.jardim.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+//HR Payroll será um cliente Ribbon que ira utilizar o balanceamento de carga para fazer requisicoes para Worker
 //Para habilitar o uso do Feign (comunicação entre MS)
+@RibbonClient(name = "hr-worker")
 @EnableFeignClients
 @SpringBootApplication
 public class HrPayrollApplication {
