a433254849d24922b3e5600104dbc085df046809
Gabriel Jardim <gabrielrlj99@outlook.com>
2021-03-15 22:02:07 -0300

    Project HR Config Server created

---
+++ hr-spring-server/src/main/java/br/jardim/hrconfigserver/HrSpringServerApplication.java
@@ -0,0 +1,13 @@
+package br.jardim.hrconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrSpringServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrSpringServerApplication.class, args);
+	}
+
+}
