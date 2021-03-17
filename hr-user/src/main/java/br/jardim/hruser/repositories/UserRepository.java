7b23e8073d8a30ca3d524597a867aec90b9b2de2
Gabriel Jardim <gabrielrlj99@outlook.com>
2021-03-17 14:43:28 -0300

    User repository, resource e zuul config

---
+++ hr-user/src/main/java/br/jardim/hruser/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package br.jardim.hruser.repositories;
+
+import br.jardim.hruser.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByEmail(String email);
+
+}
